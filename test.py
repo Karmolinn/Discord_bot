87cd081f44f729d7ac70a785e208751a45c48f1a
Karmolinn <44089590+Karmolinn@users.noreply.github.com>
2019-01-11 16:06:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext.commands import Bot
 
-TOKEN = 'NTE4MTAwNTA4NjkxNTI5NzM5.DxomZQ.AR9DQ3pSLwU7eNhF_l3wa8cME-M'
+TOKEN = ''
 
 BOT_PREFIXES = ('!')
 mybot = Bot(command_prefix=BOT_PREFIXES)
