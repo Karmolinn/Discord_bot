199f2ba9f588c84daf8a8b9572fe6da81d9c1f57
Karmo Linnumäe <kalinnu@koolimaja.sise>
2019-01-11 15:57:31 +0200

    ok

---
+++ bot.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext.commands import bot
+
+TOKEN = ''
+BOT_PREFIXES = ('!')
+mybot = Bot(command_prefix=BOT_PREFIXES)
+@bot.command()
+shutil.copyfileobj(respone.raw, out_file)
---
+++ test.py
@@ -0,0 +1,26 @@
+import os
+import discord
+from discord.ext.commands import Bot
+
+TOKEN = 'NTE4MTAwNTA4NjkxNTI5NzM5.DxomZQ.AR9DQ3pSLwU7eNhF_l3wa8cME-M'
+
+BOT_PREFIXES = ('!')
+mybot = Bot(command_prefix=BOT_PREFIXES)
+
+@mybot.command(pass_context=True)
+async def save_image(context,message):
+        imglist = os.listdir("$$$save_image")
+
+$content = file_get_contents("https://cdn.newsapi.com.au/image/v1/ddb04e16b9184a3970aa4dabcfec50a0");
+//Store in the filesystem.
+$fp = fopen("/Piltide_kaust");
+fwrite(fp, content);
+fclose(fp);
+
+mybot.run(TOKEN)
+
+
+
+
+
+
