a2980b033777b502c9d27a6868908b1a248d1eaa
Karmolinn <44089590+Karmolinn@users.noreply.github.com>
2019-02-01 15:45:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import urllib
 import requests
 import os
@@ -28,34 +27,3 @@ async def display_image(context, filename):
             await context.channel.send(file=discord.File(filename))
 
 mybot.run(TOKEN)
-=======
-import urllib
-import requests
-import os
-import discord
-from discord.ext.commands import Bot
-
-IMAGEDIRECTORY= "C:/Users/kalinnu/Desktop/Piltide_kaust"
-TOKEN = ''
-
-BOT_PREFIXES = ('!')
-mybot = Bot(command_prefix=BOT_PREFIXES)
-
-@mybot.command(pass_context=True)
-async def save_image(context,url,filename):
-
-        response = requests.get(url)
-        if response.status_code == 200:
-                with open(IMAGEDIRECTORY + "/" + filename, 'wb') as f:
-                        f.write(response.content)
-@mybot.command(pass_context=True)
-async def upload(context,filename):
-
-                img = open(IMAGEDIRECTORY + '/' + filename, 'rb')
-                bot.send_chat_action(message.chat.id, "upload")
-                bot.send_photo(message.chat.id, img)
-                img.close()
-
-        
-mybot.run(TOKEN)
->>>>>>> f76ceeaed0a4ffb0e497d0decbe2e03fba5911bf
