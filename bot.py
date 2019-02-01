330f0a2d5220277107abf5f243251e5a804fd4e5
Karmo Linnumäe <kalinnu@koolimaja.sise>
2019-02-01 15:41:15 +0200

    discord bot

--- bot.py
+++ bot.py
@@ -4,26 +4,26 @@ import os
 import discord
 from discord.ext.commands import Bot
 
-IMAGEDIRECTORY= "C:/Users/kalinnu/Desktop/Piltide_kaust"
-TOKEN = 'NTE4MTAwNTA4NjkxNTI5NzM5.DyyRdg.w3dwZYP1fCR8JytEg85-iWGk7Ok'
+IMAGEDIRECTORY = "C:/Users/kalinnu/Desktop/Piltide_kaust/"
+TOKEN = ''
 
 BOT_PREFIXES = ('!')
 mybot = Bot(command_prefix=BOT_PREFIXES)
 
-@mybot.command(pass_context=True)
-async def save_image(context,url,filename):
 
+@mybot.command(pass_context=True)
+async def save_image(context, url, filename):
         response = requests.get(url)
         if response.status_code == 200:
-                with open(IMAGEDIRECTORY + "/" + filename, 'wb') as f:
+                with open(IMAGEDIRECTORY + "/" + filename + '.jpg' , 'wb') as f:
                         f.write(response.content)
+
+
 @mybot.command(pass_context=True)
-async def upload(context,filename):
+async def display_image(context, filename):
+        filename = IMAGEDIRECTORY + filename + ".jpg"
+        print(filename)
+        with open(filename, 'rb') as fp:
+            await context.channel.send(file=discord.File(filename))
 
-                img = open(IMAGEDIRECTORY + '/' + filename, 'rb')
-                Bot.send_chat_action(message.chat.id, "upload")
-                Bot.send_photo(message.chat.id, img)
-                img.close()
-
-        
 mybot.run(TOKEN)
